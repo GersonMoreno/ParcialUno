9372183193fa0cdfefad53f3a2ef3d8eed62b7ad
grsn <gersonmore123@gmail.com>
2021-09-23 10:23:13 -0500

    H1: Aumentar la cantidad de entrada en produto: paso en rojo

--- RestauranteTest/VentaProductoTest.cs
+++ RestauranteTest/VentaProductoTest.cs
@@ -26,5 +26,22 @@ namespace RestauranteTest
             string respuesta = Producto.Entrada(cantidadEntrada: -1);
             Assert.AreEqual("La entrada del producto es incorrecta", respuesta);
         }
+        /*
+         Aumentar entrada En producto
+        H1: COMO USUARIO QUIERO REGISTRAR LA ENTRADA PRODUCTOS
+        Criterio de aceptación:
+        2. La cantidad de la entrada se le aumentará a la cantidad existente del producto
+        Dado	En el restaurante hay producto nombre: “Gaseosa”, cantidad: 10, costo: 1000, precio: 2000, utilidad:1000
+        Cuando	Se registre la entrada de 10
+        Entonces	El sistema arrojara un mensaje “La cantidad del Gaseosa aumento y es de 20”
+
+         */
+        [Test]
+        public void AumentarCantidadEntradaEnProducto()
+        {
+            var Producto = new Producto(nombre: "Gaseosa", cantidad: 10, costo: 1000, precio: 2000, utilidad: 1000);
+            string respuesta = Producto.Entrada(cantidadEntrada: 10);
+            Assert.AreEqual("La cantidad del Gaseosa aumento y es de 20", respuesta);
+        }
     }
 }
