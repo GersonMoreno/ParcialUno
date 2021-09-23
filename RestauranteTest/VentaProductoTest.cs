cbe32f37b83c8b8ffa4ccc2f82a562db4cf088fd
grsn <gersonmore123@gmail.com>
2021-09-23 11:55:31 -0500

    H2: Disminuir cantidad de productos compuestos salientes: paso en rojo

--- RestauranteTest/VentaProductoTest.cs
+++ RestauranteTest/VentaProductoTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using DomainRestaurante;
+using System.Collections.Generic;
 
 namespace RestauranteTest
 {
@@ -23,7 +24,9 @@ namespace RestauranteTest
         public void EntradaProductoDeMenosUno()
         {
             var Producto = new Producto(nombre: "Gaseosa", cantidad: 10, costo: 1000, precio: 2000, utilidad: 1000);
+
             string respuesta = Producto.Entrada(cantidadEntrada: -1);
+
             Assert.AreEqual("La entrada del producto es incorrecta", respuesta);
         }
         /*
@@ -40,7 +43,9 @@ namespace RestauranteTest
         public void AumentarCantidadEntradaEnProducto()
         {
             var Producto = new Producto(nombre: "Gaseosa", cantidad: 10, costo: 1000, precio: 2000, utilidad: 1000);
+
             string respuesta = Producto.Entrada(cantidadEntrada: 10);
+
             Assert.AreEqual("La cantidad del Gaseosa aumento y es de 20", respuesta);
         }
         /*
@@ -56,7 +61,9 @@ namespace RestauranteTest
         public void SalidaDeProductoDeMenosUno()
         {
             var Producto = new Producto(nombre: "Gaseosa", cantidad: 10, costo: 1000, precio: 2000, utilidad: 1000);
+
             string respuesta = Producto.Salida(cantidadSalida: -1);
+
             Assert.AreEqual("La cantidad de salida es incorrecta", respuesta);
         }
         /*
@@ -72,7 +79,41 @@ namespace RestauranteTest
         public void DisminuirCantidadDeProductosSalientes()
         {
             var Producto = new Producto(nombre: "Gaseosa", cantidad: 10, costo: 1000, precio: 2000, utilidad: 1000);
+
             string respuesta = Producto.Salida(cantidadSalida: 2);
+
+            Assert.AreEqual("La cantidad de Gaseosa restante es de 8", respuesta);
+        }
+        /*
+            Disminuir cantidad de productos compuestos salientes
+            H2: COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS
+            Criterio de aceptación:
+            2.3. En caso de un producto compuesto la cantidad de la salida se le disminuirá a la cantidad existente de cada uno de su ingrediente
+            Dado	En el restaurante quiere hacer un combo perro doble y tiene productos: nombre: “Gaseosa”, cantidad: 10, costo: 1000, precio: 2000, utilidad:1000
+            “Salchicha”, cantidad: 40, costo: 1000, precio: 2000, utilidad:1000
+            “Laminas queso Mozarela”, cantidad: 100, costo: 700, precio: 1500, utilidad: 800
+            “Pan de perro”, cantidad: 60, costo: 1000, precio:1500, utilidad:500
+            Cuando	Se retira una gaseosa, unas dos salchichas, dos láminas de queso y un pan de perro
+            Entonces	El sistema arrojara un mensaje “Se retiro Salchicha, habían 40 y quedaron 38.
+            Se retiro Gaseosa, habían 10 y quedaron 9.
+            Se retiro Laminas queso Mozarela, habían 100 y quedaron 98.
+            Se retiro Pan de perro, habían 60 y quedaron 59. ”
+         */
+        [Test]
+        public void DisminuirCantidadDeProductosCompuestosSalientes()
+        {
+            var Gaseosa = new Producto(nombre: "Gaseosa", cantidad: 10, costo: 1000, precio: 2000, utilidad: 1000);
+            var Salchicha = new Producto(nombre: "Salchicha", cantidad: 40, costo: 1000, precio: 2000, utilidad: 1000);
+            var QuesoMozarela = new Producto(nombre: "laminaQueso Mozarela", cantidad: 100, costo: 700, precio: 1500, utilidad: 800);
+            var PanPerro = new Producto(nombre: "Pan de Perro", cantidad: 60, costo: 1000, precio: 1500, utilidad: 500);
+            List<Producto> Ingredientes = new List<Producto>();
+            Ingredientes.Add(Gaseosa);
+            Ingredientes.Add(Salchicha);
+            Ingredientes.Add(QuesoMozarela);
+            Ingredientes.Add(PanPerro);
+
+            var Restaurante = new Restaurante(Ingredientes: Ingredientes);
+            string respuesta = Restaurante.Vender(cantidadVender: 1);
             Assert.AreEqual("La cantidad de Gaseosa restante es de 8", respuesta);
         }
     }
